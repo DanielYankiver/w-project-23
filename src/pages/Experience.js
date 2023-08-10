d49842363cacc602646100ebc55074862992bad6
Daniel Yankiver <69213153+DanielYankiver@users.noreply.github.com>
2023-08-10 09:24:04 -0400

    updates

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -3,8 +3,8 @@ import './Experience.css';
 export default function Experiences() {
   return (
     <div style={{ padding: 20 }} className='experience-container'>
-      {/* <h2>Experience View</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adip.</p> */}
+      <h2>Experience View</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
     </div>
   );
 }
