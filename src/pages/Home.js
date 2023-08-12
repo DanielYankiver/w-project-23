c459cbf94ec27e8450c1a9d17da8d0b6819f4319
Daniel Yankiver <69213153+DanielYankiver@users.noreply.github.com>
2023-08-12 15:43:44 -0400

    update and testing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,21 +6,23 @@ export default function Home() {
   return (
     <div className='home-page'>
      <div className='home-body'>
-       <img src={bbLogo} className='home-logo' alt='logo' />
-       <div className='name'>
-         Daniel Yankiver
-       </div>
-       <div className='home-description'>
-         Creative Software Engineer
-       </div>
-       <div className='home-scroll-wrapper'>
-        <div className='home-scroll-to-enter'>
-          SCROLL 
+        <div className='logo-container'>
+          <img src={bbLogo} className='home-logo' alt='logo' />
         </div>
-        <div className='home-scroll-arrow'>
-          V
+        <div className='name'>
+          Daniel Yankiver
         </div>
-       </div>
+        <div className='home-description'>
+          Creative Software Engineer
+        </div>
+        <div className='home-scroll-wrapper'>
+          <div className='home-scroll-to-enter'>
+            SCROLL 
+          </div>
+          <div className='home-scroll-arrow'>
+            V
+          </div>
+        </div>
      </div>
      <video 
         className='background-video'
